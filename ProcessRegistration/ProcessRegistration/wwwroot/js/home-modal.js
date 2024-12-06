f79d73426dcd7dc2f233d7c01a83a542466c258e
Saulo Cav <saulocavalcante25@hotmail.com>
2024-12-05 21:51:24 -0300

    Adds addition and deletion functions

--- ProcessRegistration/ProcessRegistration/wwwroot/js/home-modal.js
+++ ProcessRegistration/ProcessRegistration/wwwroot/js/home-modal.js
@@ -18,9 +18,122 @@
             })
             .then(html => {
                 modalContent.innerHTML = html;
+                attachEventListeners();
+                validateForm();
             })
             .catch(error => {
                 modalContent.innerHTML = `<p class="text-danger">Erro ao carregar: ${error.message}</p>`;
             });
     });
+});
+
+function attachEventListeners() {
+    $(document).off('change', '#State').on('change', '#State', function () {
+        var state = $(this).val();
+        if (state) {
+            $.getJSON('https://servicodados.ibge.gov.br/api/v1/localidades/estados/' + state + '/municipios')
+                .done(function (data) {
+                    var cityOptions = '<option value="">Selecione o Município</option>';
+                    $.each(data, function (key, city) {
+                        cityOptions += '<option value="' + city.id + '">' + city.nome + '</option>';
+                    });
+                    $('#City').html(cityOptions);
+                })
+                .fail(function () {
+                    alert('Erro ao carregar os municípios. Por favor, tente novamente.');
+                });
+        } else {
+            $('#City').html('<option value="">Selecione o Município</option>');
+        }
+    });
+
+    $(document).off('click', '#btnSave').on('click', '#btnSave', function () {
+        $('#loadingOverlay').show();
+        $('#btnSave').prop('disabled', true);
+
+        var name = $('#Name').val();
+        var npu = $('#NPU').val();
+        var state = $('#State').val();
+        var cityId = $('#City').val();
+
+        var formData = {
+            Name: name,
+            NPU: npu,
+            State: state,
+            City: cityId
+        };
+
+        $.ajax({
+            url: '/Process/Create',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            success: function (response) {
+                $('#loadingOverlay').hide();
+                alert(response.message);
+                $('#formProcess')[0].reset();
+                $('#actionModal').modal('hide');
+                location.reload();
+            },
+            error: function (xhr) {
+                $('#loadingOverlay').hide();
+                alert('Ocorreu um erro ao cadastrar o processo: ' + xhr.responseText);
+            },
+            complete: function () {
+                $('#btnSave').prop('disabled', false);
+            }
+        });
+    });
+
+    $(document).off('click', '#confirmDeleteButton').on('click', '#confirmDeleteButton', function () {
+        const processId = $('#processId').val();
+        $('#loadingOverlay').show();
+
+        $.ajax({
+            url: '/Process/DeleteConfirmed/' + processId,
+            type: 'POST',
+            success: function (response) {
+                $('#loadingOverlay').hide();
+                if (response.success) {
+                    alert('Processo excluído com sucesso!');
+                    $('#actionModal').modal('hide');
+                    location.reload();
+                } else {
+                    alert('Erro ao excluir o processo: ' + response.message);
+                }
+            },
+            error: function (xhr, status, error) {
+                $('#loadingOverlay').hide();
+                console.error("Status: " + status);
+                console.error("Erro: " + error);
+                alert('Ocorreu um erro ao excluir o processo: ' + xhr.responseText);
+            }
+        });
+    });
+
+
+    $(document).on('input change', '#formProcess input, #formProcess select', function () {
+        validateForm();
+    });
+}
+
+function validateForm() {
+    let isFormValid = true;
+    $('#formProcess').find('input, select').each(function () {
+        if ($(this).prop('required') && $(this).val() === "") {
+            isFormValid = false;
+            return false;
+        }
+    });
+
+    $('#btnSave').prop('disabled', !isFormValid);
+}
+
+$(document).ready(function () {
+    attachEventListeners();
+    validateForm();
 });
