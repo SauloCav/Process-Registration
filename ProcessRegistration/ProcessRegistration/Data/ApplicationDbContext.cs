cde907667cfcec3598c70d50fefae43dec9a98fe
Saulo Cav <saulocavalcante25@hotmail.com>
2024-12-05 21:32:51 -0300

    Adds the ApplicationDbContext

---
+++ ProcessRegistration/ProcessRegistration/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProcessRegistration.Models;
+using System.Collections.Generic;
+
+namespace ProcessRegistration.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
+
+        public DbSet<ProcessModel> Processes { get; set; }
+    }
+}
