a1f98a2fcbc849e566e9ef2628ec822d8bd81170
Saulo Cav <saulocavalcante25@hotmail.com>
2024-12-05 21:48:02 -0300

    Adjust the ProcessModel

--- ProcessRegistration/ProcessRegistration/Models/ProcessModel.cs
+++ ProcessRegistration/ProcessRegistration/Models/ProcessModel.cs
@@ -1,36 +1,32 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ProcessRegistration.Models
 {
     public class ProcessModel
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "O nome do processo é obrigatório.")]
-        [StringLength(100, ErrorMessage = "O nome do processo não pode exceder 100 caracteres.")]
-        public string Nome { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
 
-        [Required(ErrorMessage = "O NPU é obrigatório.")]
-        [RegularExpression(@"^\d{7}-\d{2}\.\d{4}\.\d{1}\.\d{2}\.\d{4}$", ErrorMessage = "O NPU deve estar no formato 1111111-11.1111.1.11.1111.")]
+        [Required]
+        [StringLength(25)]
         public string NPU { get; set; }
 
-        [Required(ErrorMessage = "A data de cadastro é obrigatória.")]
-        [Display(Name = "Data de Cadastro")]
-        public DateTime DataCadastro { get; set; }
+        public DateTime RegistrationDate { get; set; }
 
-        [Display(Name = "Data de Visualização")]
-        public DateTime? DataVisualizacao { get; set; }
+        public DateTime? ViewDate { get; set; }
 
-        [Required(ErrorMessage = "O UF é obrigatório.")]
-        [StringLength(2, ErrorMessage = "O UF deve conter exatamente 2 caracteres.")]
-        public string UF { get; set; }
+        [Required]
+        [StringLength(2)]
+        public string State { get; set; }
 
-        [Required(ErrorMessage = "O município é obrigatório.")]
-        public string Municipio { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string City { get; set; }
 
-        [Required(ErrorMessage = "O código do município é obrigatório.")]
-        public int CodigoMunicipio { get; set; }
+        [Required]
+        public int CityCode { get; set; }
     }
 }
