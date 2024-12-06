6a9a91d068fd8c0fb436747e269753172f8d2fef
Saulo Cav <saulocavalcante25@hotmail.com>
2024-12-05 21:53:55 -0300

    Adjust the appsettings and program

--- ProcessRegistration/ProcessRegistration/Program.cs
+++ ProcessRegistration/ProcessRegistration/Program.cs
@@ -1,19 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using ProcessRegistration.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+    pattern: "{controller=Process}/{action=Index}/{id?}");
 
 app.Run();
-
